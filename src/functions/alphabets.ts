cf2889c63f16d36079eaa15d8d3cbe9459d70d3f
Ross Williams <rossjrw@gmail.com>
2020-07-05 20:23:03 +0100

    Create function for letter retrieval

---
+++ src/functions/alphabets.ts
@@ -0,0 +1,14 @@
+import { Alphabet, Letter } from './types'
+import alphabets from '!!raw-loader!./alphabets.toml'
+
+export function getLetter(alphabets: string[], letter: string): Letter {
+  /**
+   * Finds the entry with value `letter` and returns it.
+   *
+   * @param alphabets: the alphabets from which to search. They will be
+   * combined in order of decreasing priority
+   * @param letter: the letter to search for
+   * @returns known data for the given letter
+   */
+  return null
+}
