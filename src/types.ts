9682b3b0cff446a3b7e7a2471bd460dad1cc8309
Ross Williams <rossjrw@gmail.com>
2020-07-06 11:04:17 +0100

    Type settings and config

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
 export interface State {
-  inputText: string
   alphabets: Alphabet[]
+  settings: Settings
+  structures: string[]
 }
 
 export interface Alphabet {
@@ -17,4 +18,50 @@ export interface ShermanLetter extends Letter {
   dots?: number
   vert?: number
   lines: number
+}
+
+export interface Settings {
+  text: string
+  structure: string
+  scaling: boolean
+  watermark: boolean
+  width: number
+  foregroundColour: string
+  backgroundColour: string
+  button: string
+  config: Config
+}
+
+export interface Config {
+  // TODO work out what the FUCK these mean
+  s: BlockConfig
+  p: BlockConfig
+  d: BlockConfig
+  f: BlockConfig
+  v: VowelBlockConfig
+  word: BlockConfig
+  buffer: BufferConfig
+  automatic: AutomaticConfig
+}
+
+export interface BlockConfig {
+  // What are b and a????????
+  b: number
+  a: number
+}
+
+export interface VowelBlockConfig extends BlockConfig {
+  r: number
+}
+
+export interface BufferConfig {
+  letter: number
+  word: number
+  sentence: number
+}
+
+export interface AutomaticConfig {
+  // The naming scheme here is terrible
+  scaledLessThan: number
+  spiralMoreThan: number
 }
