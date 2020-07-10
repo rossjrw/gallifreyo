ee62a055e4825f84a0177d6a98d2434c6cd0fd9a
Ross Williams <rossjrw@gmail.com>
2020-07-10 21:23:21 +0100

    Move initial settings from file to initial state

--- src/store.ts
+++ src/store.ts
@@ -9,15 +9,36 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
   state: {
-    inputText: "",
-    alphabets: [],
-  } as State,
+    text: "Gallifreyo Test",
+    alphabets: {},
+    tokenisedInput: [],
+    renderedInput: [],
+    settings: {
+      structure: [ "Automatic", "Automatic" ],
+      scaling: true, // sentence size scaling
+      watermark: true,
+      width: 1024,
+      foregroundColour: "#000000",
+      backgroundColour: "#FFFFFF",
+      button: "Translate",
+      config: {
+        s: { b: 0.9, a: 1 },
+        p: { b: 1.2, a: 1 },
+        d: { b: -0.4, a: 2 },
+        f: { b: 0, a: 0.75 },
+        v: { b: 2, a: 1, r: 0.1 },
+        word: { b: 1.2, a: 1 },
+        buffer: { letter: 0.5, word: 0.5, sentence: 0.5 },
+        automatic: { scaledLessThan: 6, spiralMoreThan: 9 },
+      },
+    },
+  }  as State,
   mutations: {
     transliterate(state: State) {
       /* state.outputText = execute(state.inputText, state.rules) */
     },
     modifyInput(state: State, newInput: string) {
-      state.inputText = newInput
+      state.text = newInput
     },
     // TODO mutation to change 'debug' settings
   },
