6f50502583b1ad340bc5ba027865601e46f02443
Ross Williams <rossjrw@gmail.com>
2020-07-17 01:22:57 +0100

    Move circle from phrase to image

--- src/components/RenderedImage.vue
+++ src/components/RenderedImage.vue
@@ -9,6 +9,9 @@
          fill="none"
          stroke="black"
          transform="scale(1,-1)">
+        <circle cx="0"
+                cy="40"
+                r="100"/>
         <RenderedPhrase v-for="phrase in phrases"
                         :key="phrase.id"
                         :phrase="phrase"/>
--- src/components/RenderedPhrase.vue
+++ src/components/RenderedPhrase.vue
@@ -2,9 +2,6 @@
   <g :transform="phrase.transform">
     <g v-for="subPhrase in phrase.phrases"
        :key="subPhrase.id">
-      <circle cx="0"
-              cy="40"
-              r="100"/>
       <RenderedLetter v-if="subPhrase.depth === 'letter'"
                       :letter="subPhrase"/>
       <RenderedPhrase v-else
