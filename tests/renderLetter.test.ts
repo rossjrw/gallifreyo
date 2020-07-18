a0df492c4042e910247b5610d63a7784e7a4b958
Ross Williams <rossjrw@gmail.com>
2020-07-18 02:59:44 +0100

    Add test and update test name

--- tests/renderLetter.test.ts
+++ tests/renderLetter.test.ts
@@ -1,8 +1,12 @@
-import { circleIntersectionPoints } from '../src/functions/render/letter'
+const { circleIntersectionPoints } = require('../src/functions/render/letter')
 
 describe("circle intersection calculator", () => {
   it("calculates intersection points", () => {
     const coords = circleIntersectionPoints(0, 0, 13, 10, 0, 13)
     expect(coords).toStrictEqual([5, 5, 12, -12])
+  })
+  it("returns NaN when intersection fails", () => {
+    const coords = circleIntersectionPoints(0, 0, 1, 10, 0, 1)
+    expect(coords).toStrictEqual([NaN, NaN, NaN, NaN])
   })
 })
