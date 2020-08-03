cb340c127d7d2303951896247024f6b505012cbe
Ross Williams <rossjrw@gmail.com>
2020-08-03 23:31:48 +0100

    Make second column seem consistent

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,25 +1,30 @@
 <template>
   <div id="app">
-    <GithubCorner id="github-corner"></GithubCorner>
+    <GithubCorner id="github-corner"/>
     <div id="ui"
          class="section">
-      <div class="container">
-        <Title id="title"
-               class="column-part"/>
-        <TextInput id="text-input"
-                   class="column-part"/>
-        <div id="fixed-section"
-             class="column-part">
-          <div id="force-square">
-            <img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEAAAAALAAAAAABAAEAAAI=">
-            <RenderedImage id="rendered-image"></RenderedImage>
-          </div>
-          <Downloads id="downloads"></Downloads>
+      <div class="buffer">
+        <div class="column-part">
+          <Title id="title"/>
         </div>
-        <SettingsOptions id="settings-options"
-                         class="column-part"/>
-        <Footer id="footer"
-                class="column-part"/>
+        <div class="column-part">
+          <TextInput id="text-input"/>
+        </div>
+        <div class="column-part">
+          <div id="fixed-section">
+            <div id="force-square">
+              <img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEAAAAALAAAAAABAAEAAAI=">
+              <RenderedImage id="rendered-image"/>
+            </div>
+            <Downloads id="downloads"/>
+          </div>
+        </div>
+        <div class="column-part">
+          <SettingsOptions id="settings-options"/>
+        </div>
+        <div class="column-part">
+          <Footer id="footer"/>
+        </div>
       </div>
     </div>
   </div>
@@ -45,6 +50,7 @@ export default Vue.extend({
 
 <style lang="scss">
 @import '~bulma/sass/utilities/_all.sass';
+@import '~bulma/sass/layout/section.sass';
 
 body {
   // Background stripes to highlight transparency
@@ -92,19 +98,54 @@ body {
 #ui {
   display: flex;
   flex-direction: column;
+}
+
+$section-side-padding: nth($section-padding, 2);
+$downloads-box-height: 5.5rem;
+$maximum-column-width: 40rem;
+
+// No buffer at mobile
+$buffer-width-mobile: 0px;
+// Buffer when the screen is too flat to hold a square
+$buffer-width-flat: calc(50vw - 100vh - #{$downloads-box-height});
+// Buffer when content exceeds 1200px
+$buffer-width-maxwidth: calc(50vw - #{$maximum-column-width});
+// The actual buffer is the largest of these
+$buffer-width: calc(max(
+  #{$buffer-width-mobile},
+  #{$buffer-width-flat},
+  #{$buffer-width-maxwidth}
+));
+
+.buffer {
+  /*
+  The left and right margins of the UI are only present when the screen is too
+  flat to accomodate the height of the square image and the downloads box
+  underneath it, but wide enough that it is using the two-column layout.
+  */
+  // calc is needed here to bypass SCSS max function (sass/sass#2378)
+  margin: 0 $buffer-width;
+}
+
+.column-part {
   @include tablet {
-    max-width: 50%;
-    left: 0;
+    // Stuff in the two-column layout should be half-width
+    width: 50%;
   }
 }
 
 #fixed-section {
   @include tablet {
+    padding: 1.5rem;
     position: fixed;
-    width: 50%;
     top: 50%;
-    right: 0;
+    right: $buffer-width;
     transform: translateY(-50%);
+    width: calc(min(
+      100vh - #{$downloads-box-height},
+      50vw - #{$section-side-padding},
+      #{$maximum-column-width}
+    ));
   }
 }
 </style>
