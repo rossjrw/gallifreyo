c343e0533b3154c5ef736611396757b6d4b57b5e
Ross Williams <rossjrw@gmail.com>
2020-08-03 23:31:21 +0100

    Make downloads box one-line

--- src/components/Downloads.vue
+++ src/components/Downloads.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="card">
     <div class="card-content">
-      <p class="title">
-      Download
-      </p>
-    </div>
-    <div class="buttons is-centered">
-      <div class="control">
-        <a class="button">
-          PNG
-        </a>
-      </div>
-      <div class="control">
-        <a class="button">
-          SVG
-        </a>
-      </div>
-      <div class="control">
-        <a class="button">
-          JSON
-        </a>
+      <div class="level">
+        <div class="level-left">
+          <p class="title">
+          Download
+          </p>
+        </div>
+        <div class="level-right">
+          <div class="buttons is-centered">
+            <div class="control">
+              <a class="button">
+                <b>.png</b>
+              </a>
+            </div>
+            <div class="control">
+              <a class="button">
+                .svg
+              </a>
+            </div>
+            <div class="control">
+              <a class="button">
+                .json
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
