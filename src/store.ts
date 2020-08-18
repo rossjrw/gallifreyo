e238eaaf907e6e617da8965874e4b00e8f0d5efd
Ross Williams <rossjrw@gmail.com>
2020-08-18 19:01:41 +0100

    Don't specify initial text in store

--- src/store.ts
+++ src/store.ts
@@ -12,7 +12,7 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
   state: {
-    text: "Gallifreyo Test",
+    text: "",
     alphabets: {},
     phrases: [],
     settings: {
